740ee2de8441d0e234c623037d59b58a4ac59a4e
Ruhi <ruhi.ahuja1001@gmail.com>
2025-03-29 19:42:49 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Ruhi Ahuja',
+    author_email= 'ruhi.ahuja1001@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
